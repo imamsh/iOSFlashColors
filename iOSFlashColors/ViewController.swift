76eb60a7c19e353244f0b2e1c8b56b073878abe6
Imam Hussain <imamsh2@gmail.com>
2015-10-25 00:42:51 -0700

    added code to change the color of the view

--- iOSFlashColors/ViewController.swift
+++ iOSFlashColors/ViewController.swift
@@ -10,17 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var i=0
+    @IBOutlet var myView: UIView!
+    @IBAction func buttonClick(sender: AnyObject) {
+        changeColor(++i)
+    }
+
     //hello world. testing 123.
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func changeColor(iteration: Int){
+        //there is another way to change the color of the view, which is self.view.backgroundColor = UIColor.yellowColor(). Here I created a outlet for the view. Want to know which apporach is good !!
+        
+        switch iteration {
+            case 1:
+                myView.backgroundColor = UIColor.yellowColor()
+            case 2:
+                myView.backgroundColor = UIColor.redColor()
+            case 3:
+                myView.backgroundColor = UIColor.purpleColor()
+            default:
+                i=0
+                myView.backgroundColor = UIColor.whiteColor()
+        }
+    }
 
 }
 
