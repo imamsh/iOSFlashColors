d66abfadbd1953e2abef129e6a3fcfe9bd253c17
Imam Hussain <imamsh2@gmail.com>
2015-10-29 15:31:15 -0700

    added a block to move around with certain speed

--- iOSFlashColors/ViewController.swift
+++ iOSFlashColors/ViewController.swift
@@ -46,6 +46,36 @@ class ViewController: UIViewController {
                     self.view.backgroundColor = UIColor.whiteColor()
                 })
         }
+        startBlockAnimation(iteration)
+    }
+    
+    func startBlockAnimation(animSpeed: Int) {
+        
+        let block = UIView()
+        block.frame = CGRect(x:50, y:50, width: 20, height: 20)
+        block.backgroundColor = UIColor.redColor()
+        self.view.addSubview(block)
+        
+            
+        // randomly create a value between 0.0 and 150.0
+        let randomYOffset = CGFloat( arc4random_uniform(150))
+            
+        // for every y-value on the bezier curve
+        // add our random y offset so that each individual animation
+        // will appear at a different y-position
+        let path = UIBezierPath()
+        path.moveToPoint(CGPoint(x: 16,y: 239 + randomYOffset))
+        path.addCurveToPoint(CGPoint(x: 301, y: 239 + randomYOffset), controlPoint1: CGPoint(x: 136, y: 373 + randomYOffset), controlPoint2: CGPoint(x: 178, y: 110 + randomYOffset))
+            
+        // create the animation
+        let anim = CAKeyframeAnimation(keyPath: "position")
+        anim.path = path.CGPath
+        anim.rotationMode = kCAAnimationRotateAuto
+        anim.repeatCount = Float.infinity
+        anim.duration = Double (animSpeed+2) % 8
+            
+        // add the animation
+        block.layer.addAnimation(anim, forKey: "animate position along path")
     }
 }
 
